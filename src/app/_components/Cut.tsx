d76f8fd60d3a9d3659768cc3dcf172859c655266
jeermuce <ernesto_mc.dev@proton.me>
2024-10-09 01:55:21 -0600

    width

--- src/app/_components/Cut.tsx
+++ src/app/_components/Cut.tsx
@@ -4,7 +4,7 @@ function Cut(zindex: { zindex: number }) {
     const zIndex = zindex.zindex;
     return (
         <div
-            className="h-12 bg-background w-full rounded-bottom-ellipse -my-2  rounded-t-none"
+            className="h-4 bg-background w-[100svw] max-w-[100svw] min-w-[100vw] rounded-bottom-ellipse -my-2  rounded-t-none"
             style={{ zIndex: zIndex }}
         />
     );
